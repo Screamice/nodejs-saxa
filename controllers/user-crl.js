f7b89f768766d0831a17afd714978cc1fda5fdd3
Misael Rico <misaelrico786@gmail.com>
2020-05-31 21:06:23 -0500

    signin module errors fixed

--- controllers/user-crl.js
+++ controllers/user-crl.js
@@ -4,25 +4,40 @@ const user = require('../models/user');
 const SALT_ROUNDS = bcrypt.genSaltSync(12);
 
 /* Insert a new user on database */
-function insertUser(data){
-    let modelUser = new user({
-        fullname: data.name,
-        username: data.user,
-        email: data.email,
-        password: bcrypt.hashSync(data.pwd, SALT_ROUNDS),
-        languaje: data.lang
+exports.insertUser = (data) => {
+    return new Promise((resolve, reject) => {
+        let modelUser = new user({
+            fullname: data.name,
+            username: data.user,
+            email: data.email,
+            password: bcrypt.hashSync(data.pwd, SALT_ROUNDS),
+            languaje: data.lang
+        });
+
+        modelUser.save((user, error) => {
+            if(error){
+                resolve(user);
+            }
+            else{
+                reject(new Error('cannot insert user in database'));
+            }
+        });
     });
+};
+/*
+function insertUser(data){
+    
 
     let flag = true;
 
-    modelUser.save(error => {
-        if(error){
-            console.error(error);
-            flag = false;
+    return(modelUser.save(error => {
+        if(error){ 
+            return false;
+        }else{
+            return true;
         }
-    });
-
-    return flag;
+    }));
 }
 
 module.exports = {insertUser};
+*/
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -17,16 +17,26 @@ exports.newUser = (req, res) => {
         );
     }
 
-    if(controller.insertUser(data)){
+    controller.insertUser(data)
+    .then(() => {
+        res.status(200);
+        res.send({message: 'user has been registered'});
+    })
+    .catch(error => {
+        console.error(error);
+        res.status(400);
+        res.send({message: 'hasta afuera no se pudo'});
+    });
+};
+
+exports.emailLogIn = (req, res) => {
+    let email = req.body.email;
+    let pwd = req.body.pwd;
+
+    if(!email && !pwd){
         return(
-            res.status(200)
-               .send({message: 'new user has been registered'})
-        );
-    }
-    else{
-        res(
             res.status(400)
-               .send({message: 'request-data is not correct'})
+               .send({message: 'the request-data is not correct'})
         );
     }
 };
